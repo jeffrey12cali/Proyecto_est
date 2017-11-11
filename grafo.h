a2c774994882afa2ddaa796883d10df782273fc8
jeffrey12cali <jeffrey12cali@javerianacali.edu.co>
2017-11-11 12:26:41 -0500

    Modificaciones .h
    prueba de modificación

--- grafo.h
+++ grafo.h
@@ -2,6 +2,7 @@
 #define __GRAFO_H
 
 #include <vector>
+#include <list>
 
 template <class T>
 class Nodo{
@@ -15,19 +16,19 @@ template <class T>
 class Grafo{
 private:
 	vector<Nodo> V; //Vertices
-	vector<vector<Nodo>> E; //Aristas
+	vector<list<Nodo>> E; //Aristas
 	vector<int> P; //Ponderaciones
 	int nAri;
 	int nVert;
 public:
 	Grafo(); //CrearGrafoVacio
-	Grafo(int nV); //CrearGrafoNoVacio 
+	Grafo(int nV); //CrearGrafoNoVacio
 	void AgregarArista(Nodo n1, Nodo n2);
 	void AgregarNodo(Nodo n1);
 	void ModificarArista(Nodo n1, Nodo n2, int valor);
 	void EliminarArista(Nodo n1, Nodo n2);
 	void EliminarNodo(Nodo n1);
-	vector<Nodo> ObtenerListaAdy(Nodo n1);
+	list<Nodo> ObtenerListaAdy(Nodo n1);
 	int ObtenerNumVertices();
 	int ObtenerNumAristas();
 	~Grafo();
