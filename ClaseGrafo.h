6618d9970bc2161c70756a5b749c9134e7578775
jeffrey12cali <jeffrey12cali@javerianacali.edu.co>
2017-11-27 18:28:02 -0500

    Documentación Clase Grafo

--- ClaseGrafo.h
+++ ClaseGrafo.h
@@ -23,72 +23,128 @@ protected:
 	int nVert;
 
 public:
-	/* Constructoras */
+	/* CONSTRUCTORAS */
+
+	/* Grafo()
+	 * Entradas: NULL; 
+	 * Salidas: NULL;
+	 * Procedimiento: Se inicializan los atributos de la clase grafo como vacíos, excepto los contadores nVert empty
+	 * nAri.
+	 */
 	Grafo(); //CrearGrafoVacio
-	/*Constructor Grafo No vacio
-	Entradas:
-	vector<T> nodos. Donde el tamaño de nodos es mayor a cero.
-	Salidas:
-	Ninguna (void).
-	Procedimiento:
-	-Crea  N nodos en el vector V (vertices).Crea N vectores vacios en el vector de aristas.
-	-Crea N vectores vacios en el vector de ponderaciones.Inicializa (0) contador de aristas y vertices.
-	*/
+
+	/* Grafo(vector<T>)
+	 * Entradas: NULL
+	 * vector<T> nodos. Donde el tamaño de nodos es mayor a cero.
+	 * Salidas: NULL
+	 * Ninguna (void).
+	 * Procedimiento:
+	 * -Crea  N nodos en el vector V (vertices).Crea N vectores vacios en el vector de aristas.
+	 * -Crea N vectores vacios en el vector de ponderaciones.Inicializa (0) contador de aristas y vertices.
+	 */
 	Grafo(vector<T>); //CrearGrafoNoVacio
 
-	/* Modificadoras */
+	/* MODIFICADORAS */
+
+	/* AgregarArista(T dato1, T dato2);
+	 * Entradas: tipo T dato1, dato2; 
+	 * Salidas: NULL;
+	 * Procedimiento: Se procede a buscar los nodos en el vector de nodos para determinar su posición, una vez
+	 * encontradas las posiciones se anexa en la última posición la nueva arista en las "listas de adyacencia" de
+	 * cada nodo. En el vector de ponderaciones relacionado con cada nodo se anexa un 0 como ponderación de esa
+	 * arista.
+	 */
 	void AgregarArista(T dato1, T dato2);
+
+	/* AgregarArista(T dato1, T dato2, int valor);
+	 * Entradas: tipo T dato1, dato2, int valor; 
+	 * Salidas: NULL;
+	 * Procedimiento: Se procede a buscar los nodos en el vector de nodos para determinar su posición, una vez
+	 * encontradas las posiciones se anexa en la última posición la nueva arista en las "listas de adyacencia" de
+	 * cada nodo. En el vector de ponderaciones relacionado con cada nodo se anexa el valor como ponderación de esa
+	 * arista.
+	 */
 	void AgregarArista(T dato1, T dato2, int valor);
 	
-	/*Agregar Nodo
-	Entradas:
-	Nodo<T>.
-	Salidas:
-	Ninguna (Void)
-	Procedimiento:
-	Anexa el nodo n1 al vector V (vertices)
-	-Anexa un vector vacio en el vector de aristas,	-Anexa un vector vacio en el vector ponderaciones
+	/* AgregarNodo(T n1)
+	 * Entradas: T n1.
+	 * Salidas: NULL.
+	 * Procedimiento:
+	 * -Anexa el nodo n1 al vector V (vertices)
+	 * -Anexa un vector vacio en el vector de aristas,	-Anexa un vector vacio en el vector ponderaciones
 	*/
 	void AgregarNodo(T n1);
+
+	/* ModificarArista(T n1, T n2, int valor); 
+	 * Entradas: tipo T n1, n2, int valor; 
+	 * Salidas: NULL;
+	 * Procedimiento: Se procede a buscar los nodos en el vector de nodos para determinar su posición, una vez
+	 * encontradas las posiciones se busca cada nodo en la lista de adyacencia del otro para determinar su posición 
+	 * en la lista de adyacencia. Posteriormente en esa posición se procede a sustuir el valor en esa posición por 
+	 * el nuevo valor ingresado como parámetro. 
+	 */
 	void ModificarArista(T n1, T n2, int valor);
-	/*Eliminar nodo
-	Entradas: nodo n1, nodo n2
-	Salidas:ninguna (void)
-	Procedimiento:
-	Dado dos nodos busca en el vector de aristas de cada nodo y elimina el nodo que se ingreso en la entrada, a su vez
-	tambien busca la posicion en el vector de pondoraciones  correspondientes a cada arista y elimina esa posicion en 
-	el vector de ponderacion y por ultimo resta en 1 la cantidad de aristas.
+
+	/*EliminarArista(T n1, T n2)
+	 * Entradas: T n1, T n2
+	 * Salidas: NULL
+	 * Procedimiento: Dado dos nodos busca en el vector de aristas de cada nodo y elimina el nodo que se ingreso en la entrada, a su vez
+	 * tambien busca la posicion en el vector de pondoraciones  correspondientes a cada arista y elimina esa posicion en 
+	 * el vector de ponderacion y por ultimo resta en 1 la cantidad de aristas.
 	*/
 	void EliminarArista(T n1,T n2);
+
+	/* EliminarNodo(T n1); 
+	 * Entradas: tipo T n1; 
+	 * Salidas: NULL;
+	 * Procedimiento: Para el nodo ingresado como parámetro se verifica que está en el vector de nodos, al encontrar
+	 * la posición del nodo se procede a comprobar que no tiene ninguna arista ni ninguna ponderación. Por último
+	 * se elimina el nodo, su "lista de adyacencia" y sus ponderaciones. 
+	 */
 	void EliminarNodo(T n1);
 
-	/* Analizadoras */
-	/*Obtener lista de Adyacencia
-	Entradas: nodo n1
-	Salidas:
-	vector<T> con N nodos adyacentes al nodo n1
-	Procedimiento:
-	Dado un nodo busca en el vector de vertices,  cuando lo encuentra, segun la posicion en la cual esta en el vector de vertices
-	entonces retorna la posicion X en E correspondiente a un vector de aristas del nodo n1.
+	/* ANALIZADORAS */
+
+	/* ObtenerListaAdy(T n1).
+	 * Entradas: nodo n1
+	 * Salidas: vector<T>
+	 * vector<T> con N nodos adyacentes al nodo n1
+	 * Procedimiento:
+	 * Dado un nodo busca en el vector de vertices,  cuando lo encuentra, segun la posicion en la cual esta en el vector de vertices
+	 * entonces retorna la posicion X en E correspondiente a un vector de aristas del nodo n1.
 	*/
 	vector<T> ObtenerListaAdy(T n1);
-	/*Obtener numero de vertices
-	Entradas: ninguna
-	Salidas: int numero.
-	Procedimiento:
-	retorna el valor de la variable nAari. (numero de aristas)
-	*/
+
+	/* ObtenerNumVercies();
+	 * Entradas: NULL; 
+	 * Salidas: Número de vertices en el grafo;
+	 * Procedimiento: Retorna el atributo nVert de la clase grafo. 
+	 */
 	int ObtenerNumVertices();
-	/*Obtener numero de aritas
-	Entradas: ninguna
-	Salidas: int numero.
-	Procedimento:
-	Retorna el valor de la variable nVert.*/
+
+	/* ObtenerNumAristas();
+	 * Entradas: NULL
+	 * Salidas: int numero.
+	 * Procedimento: Retorna el valor de la variable nVert.
+	 */
 	int ObtenerNumAristas();
+
+	/* ObtenerVertices();
+	 * Entradas: NULL; 
+	 * Salidas: vector<T> V;
+	 * Procedimiento: Retorna el vector de nodos de la clase grafo.
+	 */
 	vector<T> ObtenerVertices();
+
+	/* ObtenerPonderaciones(T n1);
+	 * Entradas: tipo T n1; 
+	 * Salidas: vector<int>;
+	 * Procedimiento: Busca la posición del nodo en el vector de nodos, y devuelve esa misma posición en el vector de 
+	 * ponderaciones.
+	 */
 	vector<int> ObtenerPonderaciones(T n1);
 
-
+	/* DESTRUCTORAS */
 	~Grafo();
 };
 /******************************************************************************************************************/
